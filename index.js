3366b17d50efaa696a1d55558767ddbb68b07e45
Eli Lawrence <eliansar1999@gmail.com>
2022-03-14 19:46:09 -0400

    modified the doggo page

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = 'left'
 
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i=0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+}
+
+let dogNames = document.querySelectorAll('.dog-name')
+for(let i=0; i < dogNames.length; i++){
+    dogNames[i].style.textAlign = 'left'
+}
+
+let footer = document.querySelector('.footer')
+footer.style.color = 'rgb(230, 187, 30)'
+
+let dogDescription = document.querySelectorAll('.dog-description')
+for(let i=0; i < dogDescription.length; i++){
+    dogDescription[i].style.color = 'rgb(222, 38, 136)'
+}
 // Select some elements...
